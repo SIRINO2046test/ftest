6819663b6a2d2232a19ece4c00ce34f59ddc5b3e
SIRINO2046test <sirino2046@naver.com>
2019-08-20 23:26:37 +0900

    test

---
+++ main.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+int main()
+{
+    uint8_t f1 = 0;
+    uint8_t f2 = 0;
+    uint8_t r1 = 0;
+    uint8_t r2 = 0;
+
+    f1 = 10;
+    f2 = 4;
+
+    r1 = add(f1,f2);
+    r2 = minus(f1,f2);
+
+    printf("%d\n",r1);
+
+    printf("%d\n",r2);
+
+    return 0;
+}
+
+
+uint8_t add(uint8_t a,uint8_t b)
+{
+    return (a+b);
+}
+
+
+uint8_t minus(uint8_t a,uint8_t b)
+{
+    if(a>b)
+    {
+        return (a-b);
+    }
+    else
+    {
+        return (b-a);
+    }
+}
---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H__
+#define MAIN_H__
+
+#include <stdint.h>
+#include <stdio.h>
+
+uint8_t add(uint8_t a,uint8_t b);
+uint8_t minus(uint8_t a,uint8_t b);
+
+#endif
